142c60215b8ad01456f0a5446338c787bb448b1f
Jason Rueckert <phlacin@gmail.com>
2020-05-19 01:02:23 -0700

    Fixed Previews for views currently using Core Data

--- BreadBox/Views/Home/Home.swift
+++ BreadBox/Views/Home/Home.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Jason Rueckert. All rights reserved.
 //
 
+import CoreData
 import SwiftUI
 
 struct Home: View {
@@ -18,64 +19,73 @@ struct Home: View {
 //
     
     var body: some View {
-        ZStack {
-            VStack {
-                HStack {
-                    Text("RECIPES")
-                        .kerning(6)
-                        .bold()
-                        .scaledFont(name: "Avenir", size: 28)
+        NavigationView {
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("RECIPES")
+                            .kerning(6)
+                            .bold()
+                            .scaledFont(name: "Avenir", size: 28)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image("icon-filter")
+                                .foregroundColor(.red)
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 60)
+                    
+                    ScrollView {
+//                                            ForEach(0..<5) { number in
+                        ForEach(self.recipes, id: \.self) { recipe in
+                            NavigationLink(destination: RecipeView(recipe: recipe)) {
+                                RecipeViewCell(recipe: recipe)
+                            }
+//                            Button(action: {
+//                                self.showRecipeView.toggle()
+//                                self.recipeNumber = number
+//                            }) {
+//                                RecipeViewCell(number: number)
+//                            }
+//                            .foregroundColor(.black)
+//                            .padding(.top)
+                        }
+                    }
                     
                     Spacer()
-                    
-                    Button(action: {
-                        
-                    }) {
-                        Image("icon-filter")
-                            .foregroundColor(.red)
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                
-                ScrollView {
-                                        ForEach(0..<5) { number in
-//                    ForEach(self.recipes, id: \.self) { recipe in
-                        Button(action: {
-                            self.showRecipeView.toggle()
-                            self.recipeNumber = number
-                        }) {
-                            RecipeViewCell(number: number)
-                        }
-                        .foregroundColor(.black)
-                        .padding(.top)
-                    }
                 }
                 
-                Spacer()
+//                RecipeView(showRecipeView: $showRecipeView, number: $recipeNumber)
+//                    .offset(y: self.showRecipeView ? 30 : 900)
             }
-            .statusBar(hidden: true)
-            
-            RecipeView(showRecipeView: $showRecipeView, number: $recipeNumber)
-                .offset(y: self.showRecipeView ? 0 : 900)
+            .navigationBarHidden(true)
+            .navigationBarTitle(Text("Home"))
+            .edgesIgnoringSafeArea([.top, .bottom])
         }
+        .statusBar(hidden: true)
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        return Home().environment(\.managedObjectContext, context)
     }
 }
 
 struct RecipeViewCell: View {
-//    var recipe: Recipe
-    var number: Int
+    var recipe: Recipe
+//    var number: Int
     
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
-                Text("sourdough \(number)".uppercased())
+                Text("\(self.recipe.name ?? "")".uppercased())
                     .bold()
                     .kerning(3)
                     .scaledFont(name: "Avenir", size: 18)
--- BreadBox/Views/Recipes/RecipeView.swift
+++ BreadBox/Views/Recipes/RecipeView.swift
@@ -6,99 +6,108 @@
 //  Copyright © 2020 Jason Rueckert. All rights reserved.
 //
 
+import CoreData
 import SwiftUI
 
 struct RecipeView: View {
-    @Binding var showRecipeView: Bool
-    @Binding var number: Int
     @State private var show = false
     @State private var viewShowing = true
-//        var recipe: Recipe
-        
-        var body: some View {
-            ZStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 30.0) {
-                    Text("Formula".uppercased())
-                        .font(.title).bold()
+    var recipe: Recipe
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 30.0) {
+                Text("Formula".uppercased())
+                    .font(.title).bold()
                     .kerning(3)
                     .bold()
                     .scaledFont(name: "Avenir", size: 28)
-                    
-                    Text("Flour: 500g".uppercased())
-                    .kerning(2)
-                    .scaledFont(name: "Avenir", size: 12)
-                    Text("Water: 500g".uppercased())
-                    .kerning(2)
-                    .scaledFont(name: "Avenir", size: 12)
-                    Text("Starter: 500g".uppercased())
-                    .kerning(2)
-                    .scaledFont(name: "Avenir", size: 12)
-                    Text("Salt: 500g".uppercased())
-                    .kerning(2)
-                    .scaledFont(name: "Avenir", size: 12)
-                }
-                .padding(30)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .offset(y: 300)
-                .background(Color.white)
-                .clipShape(RoundedRectangle(cornerRadius: 14, style: .continuous))
-                .opacity(1)
                 
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("sourdough #\(self.number)".uppercased())
-                                    .bold()
-                                    .kerning(3)
-                                    .scaledFont(name: "Avenir", size: 18)
-                                
-                                Spacer()
-                                
-                                Image(systemName: "xmark.circle.fill")
-                                    .font(.system(size: 23))
-                                    .foregroundColor(.white)
-                                    .offset(y: -20)
-                                    .onTapGesture {
-                                        print("something tapped")
-                                        self.showRecipeView = false
-                                }
-                            }
-                                
-                            Text("May 19, 2020".uppercased())
-                                .kerning(2)
-                                .foregroundColor(.white)
-                                .scaledFont(name: "Avenir", size: 12)
+                Text("Flour: \(self.recipe.flourAmount ?? "")g".uppercased())
+                    .kerning(2)
+                    .scaledFont(name: "Avenir", size: 12)
+                Text("Water: \(self.recipe.waterAmount ?? "")g".uppercased())
+                    .kerning(2)
+                    .scaledFont(name: "Avenir", size: 12)
+                Text("Starter: \(self.recipe.starterAmount ?? "")g".uppercased())
+                    .kerning(2)
+                    .scaledFont(name: "Avenir", size: 12)
+                Text("Salt: \(self.recipe.saltAmount ?? "")g".uppercased())
+                    .kerning(2)
+                    .scaledFont(name: "Avenir", size: 12)
+            }
+            .padding(30)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .offset(y: 300)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 14, style: .continuous))
+            .opacity(1)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("\(self.recipe.name ?? "")".uppercased())
+                                .bold()
+                                .kerning(3)
+                                .scaledFont(name: "Avenir", size: 18)
+                            
                             Spacer()
+                            
+                            Image(systemName: "xmark.circle.fill")
+                                .font(.system(size: 23))
+                                .foregroundColor(.white)
+                                .offset(y: -40)
+                            //                                    .onTapGesture {
+                            //                                        self.showRecipeView = false
+                            //                                }
                         }
-                        .padding(.top, show ? 50 : 16)
                         
+                        Text("May 19, 2020".uppercased())
+                            .kerning(2)
+                            .foregroundColor(.white)
+                            .scaledFont(name: "Avenir", size: 12)
                         Spacer()
                     }
+                    .padding(.top, 60)
                     
+                    Spacer()
                 }
-                .padding(20)
-                .frame(maxWidth:.infinity)
-                .frame(maxHeight:300)
-                .background(Color.gray)
-                .cornerRadius(14)
                 
-                .onTapGesture {
-                    self.show.toggle()
-                    
-                }
             }
-                .statusBar(hidden: true)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-//                .animation(.easeInOut)
-            .edgesIgnoringSafeArea(.all)
+            .padding(20)
+            .frame(maxWidth:.infinity)
+            .frame(maxHeight:300)
+            .background(Color.gray)
+            .cornerRadius(14)
+                
+            .onTapGesture {
+                self.show.toggle()
+                
+            }
         }
-
-
+        .statusBar(hidden: true)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            //                .animation(.easeInOut)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    
 }
 
 struct RecipeView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
     static var previews: some View {
-        RecipeView(showRecipeView: .constant(false), number: .constant(1))
+        let recipe = Recipe(context: moc)
+        recipe.name = "Sourdoh!"
+        recipe.flourAmount = "505"
+        recipe.waterAmount = "130"
+        recipe.starterAmount = "125"
+        recipe.saltAmount = "10"
+        
+        return NavigationView {
+            RecipeView(recipe: recipe)
+        }
     }
 }
